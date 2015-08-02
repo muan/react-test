220eaa234fe8a4d08c1e03a2ff770cb684aaad42
Mu-An Chiou <me@muanchiou.com>
2015-08-02 09:52:17 +0800

    Add input

--- main.js
+++ main.js
@@ -19,4 +19,30 @@ var Counter = React.createClass({
   }
 })
 
-React.render(<Counter count="1"></Counter>, document.body)
+var Echo = React.createClass({
+  getInitialState: function () {
+    return { echoText: null }
+  },
+
+  setName: function (evt) {
+    this.setState({ echoText: evt.target.value })
+  },
+
+  randomEmoji: function () {
+    var rand = Math.round(Math.random()*10)
+    return ["👋", "🌈", "💕", "🎈", "🍺", "🌺", "🌹", "👏", "👐", "🎉", "😉"][rand]
+  },
+
+  render: function () {
+    var emoji = this.randomEmoji()
+    var echo = this.state.echoText ? <div className="echo-text">{ emoji } { this.state.echoText } { emoji }</div> : ""
+    return (
+      <div className="echo">
+        <input type="text" className="input" onChange={ this.setName } placeholder="Type something"></input>
+        { echo }
+      </div>
+    )
+  }
+})
+
+React.render(<div class="main"><Counter count="1"></Counter><Echo></Echo></div>, document.body)
